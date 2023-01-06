11b2c859c3a468135ae8573cd11993f729a1fee8
Fernando Gomez <redchameleonapps@gmail.com>
2023-01-06 09:08:01 -0700

    Initial Commit

---
+++ ViewsAndMods/ViewsAndModsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModsApp.swift
+//  ViewsAndMods
+//
+//  Created by Fernando Gomez on 1/6/23.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
